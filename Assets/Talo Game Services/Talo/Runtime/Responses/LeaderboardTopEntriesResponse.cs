65185a8735bb3af61bbae7cc9647599fa98b4839
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-09-10 21:25:39 +0100

    add gettopentries method to leaderboardsapi

---
+++ Assets/Talo Game Services/Talo/Runtime/Responses/LeaderboardTopEntriesResponse.cs
@@ -0,0 +1,9 @@
+namespace TaloGameServices
+{
+    [System.Serializable]
+    public class LeaderboardTopEntriesResponse
+    {
+        public LeaderboardEntry[] topEntries;
+        public LeaderboardEntry[] playerEntries;
+    }
+}
